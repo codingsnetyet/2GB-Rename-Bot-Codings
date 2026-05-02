6d487051179916fd3b6a3414939cb823e36d267b
Naruto-Uzumaki-Yt <narutouzumaki3016day@gmail.com>
2026-05-02 19:12:40 +0530

    config.py

---
+++ config.py
@@ -0,0 +1,16 @@
+# ------------------------- #
+API_ID = 1234567
+API_HASH = "your_api_hash"
+BOT_TOKEN = "your_bot_token"
+# ------------------------- #
+# Don't Remove Credit 
+# Ask Doubt @AU_Bot_Discussion 
+# Owner @Mr_Mohammed_29 
+# ------------------------- #
+OWNER_ID = 123456789
+# ------------------------- #
+MONGO_URI = "your_mongodb_uri"
+# ------------------------- #
+# Don't Remove Credit 
+# Ask Doubt @AU_Bot_Discussion 
+# Owner @Mr_Mohammed_29 
