2356cc8f8049cb681b6690f9bbd00c28edb1eab1
codingsnetyet <codingsnetyet@gmail.com>
2026-05-30 22:08:28 +0530

    ffmpeg_utils.py

--- ffmpeg_utils.py
+++ ffmpeg_utils.py
@@ -35,7 +35,7 @@ def add_metadata(input_file, output_file, title, author, artist, audio, subtitle
             avoid_negative_ts="make_zero"
         )
 
-        ffmpeg.run(stream, overwrite_output=True, quiet=True)
+        ffmpeg.run(stream, overwrite_output=True)
 
         # -------- STEP 2: VALIDATE OUTPUT -------- #
         if not os.path.exists(output_file):
@@ -73,8 +73,7 @@ def add_metadata(input_file, output_file, title, author, artist, audio, subtitle
                 }
             )
 
-            ffmpeg.run(stream, overwrite_output=True, quiet=True)
-
+            ffmpeg.run(stream, overwrite_output=True)
             return output_file
 
         except Exception as e2:
